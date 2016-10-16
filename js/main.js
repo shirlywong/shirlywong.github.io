93e7b85ae9b76c0f20fe718a1d52ac8660279c70
alexouy <aouy@uwo.ca>
2016-10-16 09:38:16 -0400

    Fixed images loaded
    Images in gallery do not overlap anymore.

--- js/main.js
+++ js/main.js
@@ -1,65 +1,17 @@
-// If getting errors, using window load instead.
-
-// // works from code pen
-// $('#content').masonry({
-//     columnWidth: 300, //change to 310 when using W2 widths.
-//     truePercent: false,
-//     itemSelector: '.item',
-//     gutter: 10,
-//     isFitWidth: true,
-//     animationOptions: {
-//         duration: 700,
-//         easing: 'linear',
-//         queue: false
-//     }
-// });
-
-
-// From Tutorial
-    $(document).ready(function () {
-
-        // Initialize Masonry
-        $('#content').masonry({
-            columnWidth: 300, //change to 310 when using W2 widths.
-            truePercent: false,
-            itemSelector: '.item',
-            gutter: 10,
-            isFitWidth: true, // option in Masonry to determine largest column count possible for viewport
-            // isAnimated: !Modernizr.csstransitions,
-            animationOptions: {
-                duration: 1100,
-                easing: 'linear',
-                queue: false
-            }
-
-        }).imagesLoaded(function () {
-            $(this).masonry('reload');
-        }).imagesLoaded(function () {
-            $(this).masonry('reload');
-        });
-    });
-
-
-// // From Tutorial
-// $(window).load(function () {
-//
-//     // Initialize Masonry
-//     $('#content').masonry({
-//         columnWidth: 310,
-//         truePercent: false,
-//         itemSelector: '.item',
-//         // gutter: 10,
-//         isFitWidth: true, // option in Masonry to determine largest column count possible for viewport
-//         isAnimated: !Modernizr.csstransitions,
-//         animationOptions: {
-//             duration: 700,
-//             easing: 'linear',
-//             queue: false
-//         }
-//
-//     }).imagesLoaded(function () {
-//         $(this).masonry('reload');
-//     });
-// });
-
+var $content = $('#content');
+// initiliatize Masonry after all images hae loaded
+$content.imagesLoaded(function() {
+   $content.masonry({
+       columnWidth: 300, //change to 310 when using W2 widths.
+        truePercent: false,
+        itemSelector: '.item',
+        gutter: 10,
+        isFitWidth: true, // option in Masonry to determine largest column count possible for viewport
+        animationOptions: {
+        duration: 1100,
+            easing: 'linear',
+            queue: false
+         }
+   });
+});
 
